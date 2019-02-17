12340a27220c736d6d5e996caa36f5e0f0df7172
CuongNguyen3110 <cuongnguyen1.fit@gmail.com>
2019-02-17 11:41:24 +0700

    commit

--- js-practice/handout/Practice2/sortPractice.js
+++ js-practice/handout/Practice2/sortPractice.js
@@ -1,7 +1,16 @@
 'use strict'
 
 function sort(input) {
-  return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+  for (let i = 0; i < input.length - 1; i++) {
+    for (let j = 0; j < input.length - i; j++) {
+      if (input[j] > input[j+1]) {
+        let swapNumber = input[j];
+        input[j] = input[j+1];
+        input[j+1] = swapNumber;
+      }
+    }
+  }
+  return input;
 }
 
 module.exports = sort
